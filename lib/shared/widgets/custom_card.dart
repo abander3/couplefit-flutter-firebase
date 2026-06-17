eebab39a674b6dd394f109c7dac71ca72dfb5ab8
Anthony A <anthonya365@outlook.com>
2026-06-17 10:49:59 -0500

    Implement CustomCard widget for reusable UI component

--- lib/shared/widgets/custom_card.dart
+++ lib/shared/widgets/custom_card.dart
@@ -1,1 +1,29 @@
-custom_card.dart
+import 'package:flutter/material.dart';
+
+class CustomCard extends StatelessWidget {
+  final Widget child;
+
+  const CustomCard({
+    super.key,
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(22),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.06),
+            blurRadius: 12,
+            offset: const Offset(0, 6),
+          ),
+        ],
+      ),
+      child: child,
+    );
+  }
+}
