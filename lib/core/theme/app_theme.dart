d5ee0932c24184add66de953f8efbf3074ad7350
Anthony A <anthonya365@outlook.com>
2026-06-17 10:48:31 -0500

    Add AppColors and AppTheme classes for theming

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,1 +1,17 @@
-app_theme.dart
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const anthonyGreen = Color(0xFF1F5E3B);
+  static const aspenTeal = Color(0xFF009E9A);
+  static const background = Color(0xFFF7F9F7);
+}
+
+class AppTheme {
+  static ThemeData get lightTheme {
+    return ThemeData(
+      useMaterial3: true,
+      fontFamily: 'Roboto',
+      scaffoldBackgroundColor: AppColors.background,
+    );
+  }
+}
