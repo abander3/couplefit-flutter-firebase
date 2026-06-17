81e5a81a22804197cc58caa73c1288653186eced
Anthony A <anthonya365@outlook.com>
2026-06-17 10:49:08 -0500

    Update daily_log.dart

--- lib/features/tracking/daily_log.dart
+++ lib/features/tracking/daily_log.dart
@@ -1,1 +1,25 @@
-daily_log.dart
+class DailyLog {
+  int waterOz;
+  bool weightLogged;
+  double? weight;
+  final Map<String, bool> supplementStatus;
+
+  DailyLog({
+    this.waterOz = 0,
+    this.weightLogged = false,
+    this.weight,
+    Map<String, bool>? supplementStatus,
+  }) : supplementStatus = supplementStatus ?? {};
+}
+
+class PersonProfile {
+  final String name;
+  final int waterGoalOz;
+  final List<String> supplements;
+
+  const PersonProfile({
+    required this.name,
+    required this.waterGoalOz,
+    required this.supplements,
+  });
+}
